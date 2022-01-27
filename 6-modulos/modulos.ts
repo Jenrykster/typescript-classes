bf579fdeec4e5a5475eaaefa1e8d437fb315bdd0
João Henrique <joaoenrique.san@gmail.com>
2022-01-27 14:31:33 -0300

    feat: finish section 6

---
+++ 6-modulos/modulos.ts
@@ -0,0 +1,10 @@
+console.log('opa');
+import { areaRetangulo } from './retangulo';
+import { areaCircunferencia as circ } from './circunferencia';
+
+console.log(areaRetangulo(7, 8));
+console.log(circ(2));
+
+const { digaOi } = require('./novo');
+
+console.log(digaOi('João'));
