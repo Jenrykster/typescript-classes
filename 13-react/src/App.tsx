e5a3fc98c4d10344156771f8a43b65bdd96d78f6
João Henrique <joaoenrique.san@gmail.com>
2022-01-28 14:44:24 -0300

    feat: finish section 13

---
+++ 13-react/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Contador from './components/Contador';
+function App() {
+  return (
+    <div className='App'>
+      <Contador valorInicial={10} />
+    </div>
+  );
+}
+
+export default App;
---
+++ 13-react/src/components/ContadorValor.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+interface IContadorValorProps {
+  contador: number;
+}
+const ContadorValor = (props: IContadorValorProps) => {
+  return <p>{props.contador}</p>;
+};
+
+export default ContadorValor;
